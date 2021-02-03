af891e3e0d1e00ff47cc89d5798159b5813ae103
Tanveer <stanveer.me@gmail.com>
2021-02-03 22:27:48 +0600

    Getting ready for Deployment

--- src/app.js
+++ src/app.js
@@ -5,9 +5,8 @@ const hbs = require("hbs");
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-const port = 3000;
-
 const app = express();
+const port = process.env.PORT || 3000;
 
 //Define paths for Express Config
 
